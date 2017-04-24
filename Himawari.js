4fd317a64b0ebf4c0a192b4cbcafc69d6742395e
Crawl <icrawltogo@gmail.com>
2017-04-24 07:09:33 +0200

    Update strings

--- Himawari.js
+++ Himawari.js
@@ -18,53 +18,53 @@ client.setProvider(new SequelizeProvider(client.database));
 
 client.on('error', winston.error)
 	.on('warn', winston.warn)
-	.on('ready', () => {
+	.on('ready', () =>
 		winston.info(oneLine`
 			[DISCORD]: Client ready...
 			Logged in as ${client.user.tag}
 			(${client.user.id})
-		`);
-	})
+		`)
+	)
 	.on('disconnect', () => winston.warn('[DISCORD]: Disconnected!'))
 	.on('reconnect', () => winston.warn('[DISCORD]: Reconnecting...'))
-	.on('commandRun', (cmd, promise, msg, args) => {
+	.on('commandRun', (cmd, promise, msg, args) =>
 		winston.info(oneLine`
 			[DISCORD]: ${msg.author.tag} (${msg.author.id})
 			> ${msg.guild ? `${msg.guild.name} (${msg.guild.id})` : 'DM'}
 			>> ${cmd.groupID}:${cmd.memberName}
 			${Object.values(args)[0] !== '' || !Object.values(args).length ? `>>> ${Object.values(args)}` : ''}
-		`);
-	})
+		`)
+	)
 	.on('commandError', (cmd, err) => {
 		if (err instanceof FriendlyError) return;
 		winston.error(`[DISCORD]: Error in command ${cmd.groupID}:${cmd.memberName}`, err);
 	})
-	.on('commandBlocked', (msg, reason) => {
+	.on('commandBlocked', (msg, reason) =>
 		winston.info(oneLine`
 			[DISCORD]: Command ${msg.command ? `${msg.command.groupID}:${msg.command.memberName}` : ''}
 			blocked; User ${msg.author.tag} (${msg.author.id}): ${reason}
-		`);
-	})
-	.on('commandPrefixChange', (guild, prefix) => {
+		`)
+	)
+	.on('commandPrefixChange', (guild, prefix) =>
 		winston.info(oneLine`
 			[DISCORD]: Prefix changed to ${prefix || 'the default'}
 			${guild ? `in guild ${guild.name} (${guild.id})` : 'globally'}.
-		`);
-	})
-	.on('commandStatusChange', (guild, command, enabled) => {
+		`)
+	)
+	.on('commandStatusChange', (guild, command, enabled) =>
 		winston.info(oneLine`
 			[DISCORD]: Command ${command.groupID}:${command.memberName}
 			${enabled ? 'enabled' : 'disabled'}
 			${guild ? `in guild ${guild.name} (${guild.id})` : 'globally'}.
-		`);
-	})
-	.on('groupStatusChange', (guild, group, enabled) => {
+		`)
+	)
+	.on('groupStatusChange', (guild, group, enabled) =>
 		winston.info(oneLine`
 			[DISCORD]: Group ${group.id}
 			${enabled ? 'enabled' : 'disabled'}
 			${guild ? `in guild ${guild.name} (${guild.id})` : 'globally'}.
-		`);
-	});
+		`)
+	);
 
 client.registry
 	.registerDefaults()
@@ -75,6 +75,4 @@ client.registry
 
 client.login(token);
 
-process.on('unhandledRejection', err => {
-	winston.error(`Uncaught Promise Error: \n${err.stack}`);
-});
+process.on('unhandledRejection', err => winston.error(`Uncaught Promise Error: \n${err.stack}`));
